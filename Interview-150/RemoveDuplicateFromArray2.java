5f86aaca000150a7536ec5064d7e637305d01a95
seli <selisinha22@gmail.com>
2025-08-15 15:04:35 +0530

    q4. Remoce dupliacte from Array 2

--- Interview-150/RemoveDuplicateFromArray2.java
+++ Interview-150/RemoveDuplicateFromArray2.java
@@ -1,5 +1,5 @@
-// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
-class RemoveDuplicateFromArray1 {
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150
+class RemoveDuplicateFromArray2 {
     public static void main(String[] args) {
         int[] nums={0,0,1,1,2,3,3,4,4};
         System.out.println("Size: "+ removeDuplicate(nums));
@@ -8,9 +8,9 @@ class RemoveDuplicateFromArray1 {
         }
     }
     public static int removeDuplicate(int[] nums){
-        int j=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[i-1]){
+        int j=2;
+        for(int i=2;i<nums.length;i++){
+            if(nums[i]!=nums[j-2]){
                 nums[j++]=nums[i];
             }
         }
