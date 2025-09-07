bc8d632b18efcca3c0d257b94cd517563d366eca
seli <selisinha22@gmail.com>
2025-09-07 13:52:12 +0530

    group anagrams in java and python

---
+++ Interview-150/extra/GroupAnagrams.py
@@ -0,0 +1,8 @@
+  
+class Solution:
+    def groupAnagrams(self, strs:List[str])-> List[List[str]]:
+        res=defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
